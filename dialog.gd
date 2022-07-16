4d173890680755a29e649e248c2783caa951f654
SpeedfireV <84906236+SpeedfireV@users.noreply.github.com>
2022-07-16 20:41:58 +0200

    dialog

---
+++ dialog.gd
@@ -0,0 +1,62 @@
+extends CanvasLayer
+
+const CHAR_READ_RATE = 0.08
+
+onready var background = $MarginContainer/Background
+onready var dialog = $MarginContainer/Background/Dialog
+onready var arrow = $MarginContainer/Background/Arrow
+
+enum State {
+	READY,
+	READING,
+	FINISHED
+}
+var current_state = State.READY
+
+
+func _ready():
+	dialog.text = "Lorem Ipsum"
+	dialog.percent_visible = 1
+	arrow.visible = true
+	print("Działa")
+	current_state = State.READING
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept") and current_state == State.FINISHED:
+		change_state()
+		set_dialog_text("NEW DIALOG TEXT!!!")
+		show_dialog()
+	if current_state == State.READY:
+		hide_dialog()
+	elif current_state == State.READING and dialog.visible == false:
+		show_dialog()
+	elif current_state == State.READING and dialog.percent_visible == 1.0:
+		current_state = State.FINISHED
+	print(current_state)
+
+func set_dialog_text(_text):
+	dialog.text = _text
+
+func show_dialog():
+	dialog.visible = true
+	background.visible = true
+	arrow.visible = true
+	$Tween.interpolate_property(dialog, "percent_visible", 0.0, 1.0, len(dialog.text) * CHAR_READ_RATE, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	$Tween.start()
+	current_state = State.READING
+func hide_dialog():
+	dialog.visible = false
+	dialog.percent_visible = 0.0
+	background.visible = false
+	arrow.visible = false
+	dialog.text = ""
+
+func change_state():
+	match current_state:
+		State.READY:
+			current_state = State.READING
+		State.READING:
+			current_state = State.FINISHED
+		State.FINISHED:
+			current_state = State.READY
+
