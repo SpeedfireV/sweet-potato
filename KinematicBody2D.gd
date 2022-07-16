4a777955ebc7ba3265e3cccf0bea00d76cdd7f16
SpeedfireV <84906236+SpeedfireV@users.noreply.github.com>
2022-07-16 03:11:48 +0200

    banana animation & walking
    Zrobione chodzenie i animacja chodzenia w lewo

---
+++ KinematicBody2D.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+const UP_DIRECTION = Vector2.UP
+
+export var speed := 100.0
+
+export var jump_strength := 1500.0
+export var maximum_jumps := 2
+
+var _velocity := Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	var _horizontal_direction = (
+		Input.get_action_strength("move_right") - 
+		Input.get_action_strength("move_left")
+	)
+	var _vertical_direction = (
+				Input.get_action_strength("move_down") - 
+		Input.get_action_strength("move_up")
+	)
+	
+	_velocity.x = _horizontal_direction * speed
+	_velocity.y = _vertical_direction * speed	
+	_velocity = move_and_slide(_velocity, UP_DIRECTION)
