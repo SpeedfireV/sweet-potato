511b1d1fec6913ae0211f229cebe65cf05216318
SpeedfireV <84906236+SpeedfireV@users.noreply.github.com>
2022-07-17 02:46:13 +0200

    Movement

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -6,6 +6,11 @@ export var speed := 100.0
 
 var _velocity := Vector2.ZERO
 
+onready var body = $Body
+onready var left_shoe = $LeftShoe
+onready var right_shoe = $RightShoe
+onready var movement = $Movement
+
 func _physics_process(delta: float) -> void:
 	var _horizontal_direction = (
 		Input.get_action_strength("move_right") - 
@@ -15,6 +20,23 @@ func _physics_process(delta: float) -> void:
 				Input.get_action_strength("move_down") - 
 		Input.get_action_strength("move_up")
 	)
+	if _velocity.x != 0 or _velocity.y != 0:
+		movement.play("movement")
+
+	else:
+		movement.stop()
+		movement.play()
+		movement.stop()
+		movement.play()
+	if _velocity.x > 0:
+		body.flip_h = true
+		left_shoe.flip_h = true
+		right_shoe.flip_h = true	
+		movement.play("movement")
+	elif _velocity.x < 0:
+		body.flip_h = false
+		left_shoe.flip_h = false
+		right_shoe.flip_h = false	
 	
 	_velocity.x = _horizontal_direction * speed
 	_velocity.y = _vertical_direction * speed	
