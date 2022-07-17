1e30cebb9e7ce84d6d1f7892d9fc530ced8704c8
Kalafo <maciek97412@wp.pl>
2022-07-17 18:16:45 +0200

    lada

---
+++ Customer.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+
+export var speed := 90.0
+var go = 1.0
+
+onready var body = $Body
+onready var left_shoe = $LeftShoe
+onready var right_shoe = $RightShoe
+onready var custom = $AnimationPlayer
+
+enum State {
+	LADA,
+	STOLIK
+}
+
+var current_state = State.LADA
+var rng = RandomNumberGenerator.new()
+func _ready():
+	rng.randomize()
+	go = rng.randi_range(1,20)
+	if go < 5:
+		custom.play("Lada")
+
+
+
+
+
